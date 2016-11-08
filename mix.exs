1976ac3fc606320c05b26b3a1b9fc8c4ae1ccaf8
joshi-rohit <rohit@pingal.co>
2016-11-08 12:11:56 -0500

    add geo, tirexs, mailgun deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule PingalServer.Mixfile do
   def application do
     [mod: {PingalServer, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :tirexs]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,10 @@ defmodule PingalServer.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:geo, "~> 1.1"},
+     {:tirexs, "~> 0.8"},
+    {:mailgun, "~> 0.1.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
