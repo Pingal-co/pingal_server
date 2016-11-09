8e871ff3ee257a00a0a2a05cd033153fa2eae0ae
joshi-rohit <rohit@pingal.co>
2016-11-09 09:05:11 -0500

    alter tables: add column, index

--- priv/repo/migrations/20161108203957_alter_all_tables.exs
+++ priv/repo/migrations/20161108203957_alter_all_tables.exs
@@ -3,5 +3,26 @@ defmodule PingalServer.Repo.Migrations.AlterAllTables do
 
   def change do
 
+    alter table(:thoughts) do
+      add :geom, :geometry
+    end
+
+    # index for group by / order by clauses other than primary key
+    create index(:users, [:name])
+    create index(:rooms, [:name])
+    create index(:apps, [:name])
+    create index(:networks, [:name])  
+    create index(:events, [:event])
+   
+   # sorted index in descending
+    create index(:rooms, ["display_start_time DESC NULLS LAST"])
+    create index(:slides, ["display_start_time DESC NULLS LAST"])
+
+    # partial index
+    execute("CREATE INDEX sponsored_rooms ON rooms(sponsored) WHERE sponsored = true")
+    execute("CREATE INDEX sponsored_slides ON slides(sponsored) WHERE sponsored = true")
+
+
+
   end
 end
