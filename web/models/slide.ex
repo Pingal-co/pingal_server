9e154f0fa7eb34c07ff345e1be51ef3d8bcacae5
joshi-rohit <rohit@pingal.co>
2016-11-09 09:30:59 -0500

    slide model

--- web/models/slide.ex
+++ web/models/slide.ex
@@ -1,5 +1,6 @@
 defmodule PingalServer.Slide do
   use PingalServer.Web, :model
+  alias PingalServer.Slide 
 
   schema "slides" do
     field :body, :string
@@ -10,6 +11,8 @@ defmodule PingalServer.Slide do
     belongs_to :user, PingalServer.User
     belongs_to :room, PingalServer.Room
 
+    has_many :events, PingalServer.Event
+
     timestamps()
   end
 
@@ -18,7 +21,57 @@ defmodule PingalServer.Slide do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:body, :display_start_time, :display_end_time, :public, :sponsored])
-    |> validate_required([:body, :display_start_time, :display_end_time, :public, :sponsored])
+    |> cast(params, [:body, :display_start_time, :display_end_time, :public, :sponsored, :user_id, :room_id])
+    |> validate_required([:body])
+  end
+
+  @doc """
+  Insert a slide based on the `slide` struct.
+  """
+  def insert_slide(%{} = slide) do
+    %Slide{}
+    |> changeset(slide)
+    |> Repo.insert!
+  end
+
+  def get_slides do
+    query = from s in Slide,
+      select: %{id: s.id, body: s.body, public: s.public, sponsored: s.sponsored, user_id: s.user_id, room_id: s.room_id, team_id: s.team_id}
+
+    query
+    |> Repo.all
+  end
+
+  def get_slides(:room, room_id) do
+    query = from s in Slide,
+      select: %{id: s.id, body: s.body, public: s.public, sponsored: s.sponsored, user_id: s.user_id, room_id: s.room_id, team_id: s.team_id},
+      where: s.room_id == ^room_id
+
+    query
+    |> Repo.all
+  end
+
+  def get_slides(:user, user_id) do
+    query = from s in Slide,
+      select: %{id: s.id, body: s.body, public: s.public, sponsored: s.sponsored, user_id: s.user_id, room_id: s.room_id, team_id: s.team_id},
+      where: s.user_id == ^user_id
+
+    query
+    |> Repo.all
+  end
+
+  def get_slide(slide_id) do
+    Repo.get!(Slide, slide_id)
+  end
+
+  def update_slide(%{"id" => slide_id} = slide_changes) do
+    Repo.get!(Slide, slide_id)
+    |> changeset(slide_changes)
+    |> Repo.update!
+  end
+
+  def delete_slide(slide_id) do
+    Repo.get!(Slide, slide_id)
+    |> Repo.delete
   end
 end
