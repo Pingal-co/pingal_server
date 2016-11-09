fb1cc084ccdcf492893bfdcc8519bba668755581
joshi-rohit <rohit@pingal.co>
2016-11-09 12:24:38 -0500

    coherence router changes

--- web/router.ex
+++ web/router.ex
@@ -1,5 +1,6 @@
 defmodule PingalServer.Router do
   use PingalServer.Web, :router
+  use Coherence.Router      
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -7,22 +8,52 @@ defmodule PingalServer.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session
+  end
+
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true  
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  pipeline :apiprotected do
+    plug :accepts, ["json"]
+    plug Coherence.Authentication.Session, protected: true 
+  end
+
+  scope "/" do
+    pipe_through :browser
+    coherence_routes
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
+
   scope "/", PingalServer do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+  end
+
+  scope "/", PingalServer do
+    pipe_through :protected
     resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
   scope "/api", PingalServer do
-     pipe_through :api
+     pipe_through :apiprotected
      resources "/networks", NetworkController, except: [:new, :edit]
      resources "/rooms", RoomController, except: [:new, :edit]
      resources "/thoughts", ThoughtController, except: [:new, :edit]
@@ -30,3 +61,5 @@ defmodule PingalServer.Router do
      resources "/slides", SlideController, except: [:new, :edit]
    end
 end
+
+
