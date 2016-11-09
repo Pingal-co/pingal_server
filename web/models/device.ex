7f05aee6d0e10bbb0eef93c5bca2d202822d8d88
joshi-rohit <rohit@pingal.co>
2016-11-09 09:18:20 -0500

    device model

--- web/models/device.ex
+++ web/models/device.ex
@@ -18,7 +18,54 @@ defmodule PingalServer.Device do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:device, :brand, :name, :user_agent, :locale, :country])
-    |> validate_required([:device, :brand, :name, :user_agent, :locale, :country])
+    |> cast(params, [:device, :brand, :name, :user_agent, :locale, :country, :user_id])
+    |> validate_required([:device])
+  end
+
+  @doc """
+  Insert a device based on the `device` struct.
+  """
+  def insert_device(%{} = device) do
+    %Device{}
+    |> changeset(device)
+    |> Repo.insert!
+  end
+
+  def get_devices do
+    query = from d in Device,
+      select: %{ id: d.id, device: d.device, user_id: d.user_id, brand: d.brand, name: d.name, user_agent: d.user_agent, locale: d.locale, country: d.country }
+
+    query
+    |> Repo.all
+  end
+
+  def get_devices(user_id) do
+    query = from d in Device,
+      select: %{ id: d.id, device: d.device, user_id: d.user_id, brand: d.brand, name: d.name, user_agent: d.user_agent, locale: d.locale, country: d.country },
+      where: d.user_id == ^user_id
+
+    query
+    |> Repo.all
+  end
+
+  def get_device(device) do
+    query = from d in Device,
+      select: %{ id: d.id, device: d.device, user_id: d.user_id, brand: d.brand, name: d.name, user_agent: d.user_agent, locale: d.locale, country: d.country },
+      where: d.device == ^device
+
+    query
+    |> Repo.all
+  end
+
+  def update_device(%{device: device} = device_changes) do
+    Repo.get_by!(Device, device: device) 
+    |> changeset(device_changes)
+    |> Repo.update!
+  end
+
+
+  def delete_device(device) do
+    Repo.get!(Device, device)
+    |> Repo.delete
   end
 end
