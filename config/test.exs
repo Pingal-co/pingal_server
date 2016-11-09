b1e8a2d33c10419358e957f635582a4b2e2868b7
joshi-rohit <rohit@pingal.co>
2016-11-09 09:59:10 -0500

    config

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :pingal_server, PingalServer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "rohit",
+  password: "Pingal123",
   database: "pingal_server_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
