e40da9ede15faa394ccb1043df25a3dcf46d8d54
joshi-rohit <rohit@pingal.co>
2016-11-09 09:19:22 -0500

    event model

--- web/models/event.ex
+++ web/models/event.ex
@@ -15,7 +15,43 @@ defmodule PingalServer.Event do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:event])
+    |> cast(params, [:event, :user_id, :room_id, :slide_id])
     |> validate_required([:event])
+  end
+
+  @doc """
+  Insert a user based on the `slide` struct.
+  """
+  def insert_event(name) do
+    %Event{}
+    |> changeset(name)
+    |> Repo.insert!
+  end
+
+  def get_events do
+    query = from e in Event,
+      select: {e.id, e.name}
+
+    query
+    |> Repo.all
+  end
+
+  def get_event(id) when is_number(id) do
+    Repo.get!(Event, id)
+  end
+
+  def get_event(name) do
+      Repo.get_by!(Event, name: name)
+  end
+
+  def update_event(%{"id" => event_id} = changes) do
+    Repo.get!(Event, event_id)
+    |> changeset(changes)
+    |> Repo.update!
+  end
+
+  def delete_event(event_id) do
+    Repo.get!(Event, event_id)
+    |> Repo.delete
   end
 end
