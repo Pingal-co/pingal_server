42fc3432ac3ec6b00eb0dbf225b5c9637af2effb
joshi-rohit <rohit@pingal.co>
2016-11-09 09:21:01 -0500

    network model

--- web/models/network.ex
+++ web/models/network.ex
@@ -3,6 +3,7 @@ defmodule PingalServer.Network do
 
   schema "networks" do
     field :name, :string
+    has_many :rooms, PingalServer.Room
 
     timestamps()
   end
@@ -14,5 +15,41 @@ defmodule PingalServer.Network do
     struct
     |> cast(params, [:name])
     |> validate_required([:name])
+  end
+
+  @doc """
+  Insert a user based on the `slide` struct.
+  """
+  def insert_network(params) do
+    %Network{}
+    |> changeset(params)
+    |> Repo.insert!
+  end
+
+  def get_networks do
+    query = from n in Network,
+      select: {n.id, n.name, }
+
+    query
+    |> Repo.all
+  end
+
+  def get_network(id) when is_number(id) do
+    Repo.get!(Network, id)
+  end
+
+  def get_network(name) do
+      Repo.get_by!(Network, name: name)
+  end
+
+  def update_network(%{"id" => network_id} = changes) do
+    Repo.get!(Network, network_id)
+    |> changeset(changes)
+    |> Repo.update!
+  end
+
+  def delete_network(network_id) do
+    Repo.get!(Network, network_id)
+    |> Repo.delete
   end
 end
