f1884f89a5c0ce208b25cf463e376b0e0bc2ee8a
joshi-rohit <rohit@pingal.co>
2016-11-10 19:16:43 -0500

    elixir nanomsg test

--- config/config.exs
+++ config/config.exs
@@ -38,3 +38,15 @@ config :guardian, Guardian,
   secret_key: System.get_env("GUARDIAN_SECRET_KEY"),
   serializer: PingalServer.GuardianSerializer
 
+config :exns, nanoservices: [
+
+  [name: :math_service,
+   address: "ipc:///tmp/math-service.sock",
+   timeout: 5000,
+   workers: 10],
+
+  [name: :string_service,
+   address: "ipc:///tmp/string-service.sock",
+   timeout: 5000,
+   workers: 10,
+   encoder: "msgpack"]]
